2e8a03dd6d729a7ab2b6d7a251d2d234535b43e2
Jhayro Jhordan Roncal Tacilla <jhayro2017@gmail.com>
2022-10-21 01:33:21 -0500

    Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  basePath:string=environment.basePathUser;
+  actualUser!: User;
+  auth!: Boolean;
 
+  public loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private userService: UserService,
+    private http:HttpClient,
+    private router:Router,
+    private snackBar: MatSnackBar,
+
+    ){}
   ngOnInit(): void {
+    this.loginForm=this.formBuilder.group({
+      email:[''],
+      password:['']
+    })
+  }
+
+
+  login(){
+    this.http.get<any>(this.basePath)
+    .subscribe(res=>{
+      const user=res.find((a:any)=>{
+        
+        if(a.email===this.loginForm.value.email && a.password === this.loginForm.value.password){
+          this.userService.setActualIde(a.id);
+          this.auth=true;
+        }
+        return this.auth
+      });
+      if(user){
+        this.snackBar.open('Ingresaste', '', {
+          duration: 3000,
+        });
+        this.loginForm.reset();
+        this.router.navigate(['/panel']);
+      }else{
+        this.snackBar.open('El usuario es Incorrecto', '', {
+          duration: 3000,
+        });
+      }
+    },err=>{
+      this.snackBar.open('Ingresa tus credenciales', '', {
+        duration: 3000,
+      });
+    })
+
   }
 
 }
